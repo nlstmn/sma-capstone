b9b0fc19af4deb15d0337bb92da68a100f79437b
Dario Pejić <pejic.dario@hotmail.com>
2018-11-15 19:37:14 +0200

    Revert "Created models for brinfluence app"
    This reverts commit e0eb65062c664efa4656994c2f49e7a7f9a35a21.

--- sma_core/brinfluence/admin.py
+++ sma_core/brinfluence/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from django.apps import apps
-
 
 # Register your models here.
-
-from django.contrib import admin
-from django.apps import apps
-
-app = apps.get_app_config('brinfluence')
-
-for model_name, model in app.models.items():
-    admin.site.register(model)
--- sma_core/sma_core/settings.py
+++ sma_core/sma_core/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts',
-    'brinfluence',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
